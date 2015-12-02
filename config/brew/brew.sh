cdd984a9bac6334d315b9064e3182e15955781fb
Francisco Albarran <pahko.xd@gmail.com>
2015-12-01 23:08:36 -0500

    install brew first

--- config/brew/brew.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# installs brew
-if ! type -P 'brew' &>/dev/null; then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    brew doctor
-fi
--- kickstart.sh
+++ kickstart.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-# install brew http://brew.sh/
-echo "Installing Brew"
-source ./config/brew/brew.sh
-
-echo "Installing pyenv"
-source ./config/pyenv/pyenv.sh
+# install brew and brew packages
+source ./brew.sh
